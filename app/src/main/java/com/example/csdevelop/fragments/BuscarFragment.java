078dcfe04f115cb0be65a071082784bd7fafc373
LaraHermida <larahermidapinto@gmail.com>
2023-05-30 21:47:26 +0200

    si metes numeros, busca en el campo fecha pero hay que retocarlo todavia

--- app/src/main/java/com/example/csdevelop/fragments/BuscarFragment.java
+++ app/src/main/java/com/example/csdevelop/fragments/BuscarFragment.java
@@ -6,27 +6,20 @@ import android.os.Bundle;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.example.csdevelop.DetalleConcierto;
-import com.example.csdevelop.MainActivity;
 import com.example.csdevelop.R;
 import com.example.csdevelop.adapter.ConciertoAdapter;
-import com.example.csdevelop.login.LogIn;
 import com.example.csdevelop.model.Concierto;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class BuscarFragment extends Fragment {
 
@@ -41,38 +34,44 @@ public class BuscarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //creamos una vista con lo que habia en el return y ya podemos programar
         View vista=inflater.inflate(R.layout.fragment_buscar, container, false);
 
         firestore=FirebaseFirestore.getInstance();
-
 
         rv=vista.findViewById(R.id.rv);
         SearchView searchView = vista.findViewById(R.id.search);
         searchView.setIconifiedByDefault(false);
         searchView.setQueryHint("Introduzca artista, grupo, lugar...");
 
-
-
         setUpRecyclerView();
 
-        // Agregar listener al SearchView
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 return false;
             }
 
+            @Override
             public boolean onQueryTextChange(String newText) {
                 String searchTerm = newText.toLowerCase();
-                if (!searchTerm.isEmpty()) {
-                    searchTerm = searchTerm.substring(0, 1).toUpperCase() + searchTerm.substring(1);
-                }
 
-                Query newQuery = firestore.collection("conciertos")
-                        .orderBy("nombre")
-                        .startAt(searchTerm)
-                        .endAt(searchTerm + "\uf8ff");
+
+                Query newQuery;
+                if (isNumeric(searchTerm)) {
+                    newQuery = firestore.collection("conciertos")
+                            .orderBy("fecha")
+                            .startAt(searchTerm)
+                            .endAt(searchTerm + "\uf8ff");
+                } else {
+                    if (!searchTerm.isEmpty()) {
+                        searchTerm = searchTerm.substring(0, 1).toUpperCase() + searchTerm.substring(1);
+                    }
+
+                    newQuery = firestore.collection("conciertos")
+                            .orderBy("nombre")
+                            .startAt(searchTerm)
+                            .endAt(searchTerm + "\uf8ff");
+                }
 
                 FirestoreRecyclerOptions<Concierto> newOptions =
                         new FirestoreRecyclerOptions.Builder<Concierto>()
@@ -88,6 +87,16 @@ public class BuscarFragment extends Fragment {
         return vista;
     }
 
+
+    private boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
     private void setUpRecyclerView() {
         Query originalQuery = firestore.collection("conciertos")
                 .orderBy("nombre")
@@ -101,22 +110,19 @@ public class BuscarFragment extends Fragment {
 
 
         adapter = new ConciertoAdapter(firestoreRecyclerOptions);
-        rv.setLayoutManager(new GridLayoutManager(getContext(),2));
+        rv.setLayoutManager(new GridLayoutManager(getContext(), 2));
 
-        adapter.notifyDataSetChanged();
         rv.setAdapter(adapter);
 
         adapter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Toast.makeText(getContext(),"Seleccion",Toast.LENGTH_SHORT).show();
-                int posicion =rv.getChildAdapterPosition(v);
-                Concierto c=adapter.getItem(posicion);
+                int posicion = rv.getChildAdapterPosition(v);
+                Concierto c = adapter.getItem(posicion);
                 lanzarDetalle(c);
             }
         });
     }
-
 
     @Override
     public void onStart() {
@@ -130,7 +136,7 @@ public class BuscarFragment extends Fragment {
         adapter.stopListening();
     }
 
-    public void lanzarDetalle(Concierto concierto){
+    public void lanzarDetalle(Concierto concierto) {
         Intent intent = new Intent(getContext(), DetalleConcierto.class);
         intent.putExtra("concierto", concierto);
         startActivity(intent);
